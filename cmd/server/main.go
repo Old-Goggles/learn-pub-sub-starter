a578b99d9d0c2138422d466a632f9e11acdf4e62
Nathan Conlon <nconlon@pm.me>
2026-05-08 12:44:22 -0500

    feat: publish pause state to peril_direct exchange via RabbitMQ
    - Open channel from RabbitMQ connection in server
    - Add internal/pubsub package with generic PublishJSON function
    - Publish PlayingState{IsPaused: true} on server startup

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	connectionString := "amqp://guest:guest@localhost:5672/"
+
+	conn, err := amqp.Dial(connectionString)
+	if err != nil {
+		log.Fatalf("Could not connect to RabbitMQ: %v", err)
+	}
+
+	defer conn.Close()
+
+	channel, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("Unable to open channel %v", err)
+	}
+
+	pubsub.PublishJSON(
+		channel,
+		routing.ExchangePerilDirect,
+		routing.PauseKey,
+		routing.PlayingState{
+			IsPaused: true,
+		})
+
+	fmt.Println("Peril server is connected and ready to rumble!")
+
+	signalChan := make(chan os.Signal, 1)
+
+	signal.Notify(signalChan, os.Interrupt)
+
+	<-signalChan
+
+	fmt.Println("The battle is over... shutting down Peril server.")
 }
