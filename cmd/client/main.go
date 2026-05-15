8ed4ddd89ca6ed6477b91d5c30a63b9827a826ff
Nathan Conlon <nconlon@pm.me>
2026-05-15 10:48:09 -0500

    feat: add client REPL with spawn, move, status, help, spam, and quit commands

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
-	"os/signal"
 
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
@@ -35,9 +33,47 @@ func main() {
 
 	fmt.Printf("Queue %v declared and bound!\n", result.Name)
 
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
-	<-signalChan
-	fmt.Println("Goodbye!")
+	gameState := gamelogic.NewGameState(userName)
 
+	for {
+		words := gamelogic.GetInput()
+		if len(words) == 0 {
+			continue
+		}
+
+		switch words[0] {
+		case "spawn":
+			err := gameState.CommandSpawn(words)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+
+		case "move":
+			move, err := gameState.CommandMove(words)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+
+			fmt.Printf("%v was successful.", move)
+
+		case "status":
+			gameState.CommandStatus()
+
+		case "help":
+			gamelogic.PrintClientHelp()
+
+		case "spam":
+			fmt.Println("Spamming not allowed yet!")
+
+		case "quit":
+			gamelogic.PrintQuit()
+			return
+
+		default:
+			fmt.Println("Command Not Found")
+			continue
+		}
+	}
 }
